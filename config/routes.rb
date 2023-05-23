9478ecc08e3c76236d6021fd4319fa0992988d0a
Etienne Leguay <etienneleguay@icloud.com>
2023-05-23 16:58:11 -0400

    Articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/articles", to: "articles#index"
+
+  get "/articles/new", to: "articles#new", as: :new_article
+  post "/articles", to: "articles#create"
+
+  get "/articles/:id/edit", to: "articles#edit", as: :edit_article
+  patch "/articles/:id", to: "articles#update"
+
+  get "/articles/:id", to: "articles#show", as: :article
+
+  delete '/articles/:id', to: 'articles#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times {
+  @article = Article.new(title: Faker::Cannabis.cannabinoid, content: Faker::Cannabis.health_benefit)
+  @article.save
+}
